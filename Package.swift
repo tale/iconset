6b5eb879474db70c8723a0303c0f52cbf51a3f87
Aarnav Tale <aarnav@tale.me>
2021-11-12 00:23:21 -0500

    chore: cleanup Package.swift

--- Package.swift
+++ Package.swift
@@ -1,23 +1,24 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "iconset",
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.0"),
-    ],
-    targets: [
-        .executableTarget(
-            name: "iconset",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser")
-            ],
-            path: "Sources",
-            swiftSettings: [
-                .unsafeFlags(["--disable-sandbox --arch arm64 --arch x86_64"], .when(platforms: nil, configuration: .release))
-            ]
-        )
-    ]
+	name: "Iconset",
+	platforms: [
+		.macOS(.v10_15)
+	],
+	dependencies: [
+		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+		.package(url: "https://github.com/luoxiu/Chalk", from: "0.2.0")
+	],
+	targets: [
+		.executableTarget(
+			name: "Iconset",
+			dependencies: [
+				.product(name: "ArgumentParser", package: "swift-argument-parser"),
+				.product(name: "Chalk", package: "Chalk")
+			],
+			path: "Sources"
+		)
+	]
 )
