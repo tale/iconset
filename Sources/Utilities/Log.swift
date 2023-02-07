fa09ab9bfb8bb12e15253bac3e9d55c69af5937f
Aarnav Tale <aarnav@tale.me>
2023-02-07 18:57:54 -0500

    chore: add debug logging

--- Sources/Utilities/Log.swift
+++ Sources/Utilities/Log.swift
@@ -14,17 +14,25 @@ class Log {
 		let prefix = ck.bold.on(ck.dim.on("[") + ck.yellowBright.on("!") + ck.dim.on("]"))
 		print("\(prefix) \(message)")
 	}
-	
+
 	public class func info(_ message: String) {
 		let prefix = ck.bold.on(ck.dim.on("[") + ck.blueBright.on("i") + ck.dim.on("]"))
 		print("\(prefix) \(message)")
 	}
-	
+
 	public class func error(_ message: String) {
 		let prefix = ck.bold.on(ck.dim.on("[") + ck.redBright.on("x") + ck.dim.on("]"))
 		print("\(prefix) \(message)")
 	}
-	
+
+	public class func debug(_ message: String) {
+		let prefix = ck.bold.on(ck.dim.on("[") + ck.gray.on("*") + ck.dim.on("]"))
+
+		if ProcessInfo.processInfo.environment["DEBUG"] != nil {
+			print("\(prefix) \(message)")
+		}
+	}
+
 	public class func prompt(_ message: String) -> String {
 		let prefix = ck.bold.on(ck.dim.on("[") + ck.cyanBright.on("?") + ck.dim.on("]"))
 		return "\(prefix) \(message)"
--- Sources/Utilities/Resource.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Aarnav Tale on 2/7/23.
-//
-
-import Foundation
